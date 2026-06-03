fa6feaf832afab5a385e7abc924776e30fdc2472
GnuBurn <ausakorn1909@gmail.com>
2026-06-03 21:45:46 +0700

    Add 'basic dependencies management'

---
+++ 1_DotnetDependencies/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.NetworkInformation;
+using Humanizer;
+
+static void HumanizeQuantities()
+{
+  Console.WriteLine("fly".ToQuantity(0));
+  Console.WriteLine("fly".ToQuantity(1));
+  Console.WriteLine("fly".ToQuantity(5));
+}
+
+static void HumanizeDates()
+{
+  Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+  Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+  Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+  Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+}
+
+Console.WriteLine("Quantities:");
+HumanizeQuantities();
+
+Console.WriteLine("\nDate/Time Manipulation:");
+HumanizeDates();
+
