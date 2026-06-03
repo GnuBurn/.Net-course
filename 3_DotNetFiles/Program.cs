b05c947136e561900b49fd1c8d7c565d99059856
GnuBurn <ausakorn1909@gmail.com>
2026-06-04 00:23:08 +0700

    Add 'basic file management'

---
+++ 3_DotNetFiles/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);
+var salesFiles = FindFiles(storesDirectory);
+var salesTotal = CalculateSalesTotal(salesFiles);
+
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+foreach (var file in salesFiles)
+{
+  Console.WriteLine(file);
+}
+
+IEnumerable<string> FindFiles(string folderName)
+{
+  List<string> salesFiles = new List<string>();
+
+  var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+  foreach (var file in foundFiles)
+  {
+    var extension = Path.GetExtension(file);
+    if (extension == ".json")
+    {
+      salesFiles.Add(file);
+    }
+  }
+  return salesFiles;
+}
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+  double salesTotal = 0;
+
+  foreach (var file in salesFiles)
+  {
+    string salesJson = File.ReadAllText(file);
+    SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+    salesTotal += data?.Total ?? 0;
+  }
+  return salesTotal;
+}
+
+record SalesData (double Total);
+
+// Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
+// Console.WriteLine(Path.Combine("stores", "201"));
+
+// Console.WriteLine(Path.GetExtension("sales.json")); get file type
+
+// string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales.json";
+
+// FileInfo info = new FileInfo(fileName);
+
+// Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}");
+
